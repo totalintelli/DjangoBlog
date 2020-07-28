eee6cd1b507d0e94f082256275d4a6df4f31d615
totalintelli <blueintelli@gmail.com>
2020-07-28 17:55:08 +0900

    html 파일에 게시물 출력.

--- notebook/views.py
+++ notebook/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    # return render(request, 'notebook/index.html', {})
-    return render(request, 'notebook/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # {} 템플릿을 사용하기 위해 매개변수를 추가
+    # {'posts': posts}
+    return render(request, 'notebook/post_list.html', {'posts': posts})
